e48980f52d0a28333938bd0e1eff976bcc946b03
Gurpreet Singh <Guppst3r@gmail.com>
2015-07-13 12:13:34 -0400

    Added Photoshop Image Gallery

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -22,19 +22,49 @@ angular
 			})
 			.when('/about', {
 				title        : 'About',
-				templateUrl  : 'views/about.html',
+				templateUrl  : 'views/about/index.html',
 				controller   : 'AboutCtrl',
 				controllerAs : 'about'
 			})
-			.when('/projects', {
+			.when('/projects/current', {
 				title        : 'Projects',
-				templateUrl  : 'views/projects.html',
+				templateUrl  : 'views/projects/current.html',
+				controller   : 'ProjectsCtrl',
+				controllerAs : 'projects'
+			})
+			.when('/projects/java', {
+				title        : 'Projects',
+				templateUrl  : 'views/projects/java.html',
+				controller   : 'ProjectsCtrl',
+				controllerAs : 'projects'
+			})
+			.when('/projects/cplusplus', {
+				title        : 'Projects',
+				templateUrl  : 'views/projects/cplusplus.html',
+				controller   : 'ProjectsCtrl',
+				controllerAs : 'projects'
+			})
+			.when('/projects/photoshop', {
+				title        : 'Projects',
+				templateUrl  : 'views/projects/photoshop.html',
+				controller   : 'ProjectsCtrl',
+				controllerAs : 'projects'
+			})
+			.when('/projects/flash', {
+				title        : 'Projects',
+				templateUrl  : 'views/projects/flash.html',
+				controller   : 'ProjectsCtrl',
+				controllerAs : 'projects'
+			})
+			.when('/projects/other', {
+				title        : 'Projects',
+				templateUrl  : 'views/projects/other.html',
 				controller   : 'ProjectsCtrl',
 				controllerAs : 'projects'
 			})
 			.when('/contact', {
 				title        : 'Contact',
-				templateUrl  : 'views/contact.html',
+				templateUrl  : 'views/contact/index.html',
 				controller   : 'ContactCtrl',
 				controllerAs : 'contact'
 			})
